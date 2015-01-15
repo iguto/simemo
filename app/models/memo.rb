381962253f79c39fafeea463c5365f7404de7d66
iguto <clc.iguto@gmail.com>
2015-01-16 01:34:39 +0900

    annotate

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: memos
+#
+#  id         :integer          not null, primary key
+#  title      :string(255)
+#  content    :text
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Memo < ActiveRecord::Base
 end
