72d6702ed9b07e4c03d94fde129a79d1c8c3fb56
iguto <clc.iguto@gmail.com>
2015-01-17 17:08:09 +0900

    メモの作成

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -2,4 +2,20 @@ class MemosController < ApplicationController
   def index
     @memos = Memo.all
   end
+
+  def new
+    @memo = Memo.new
+  end
+
+  def create
+    @memo = Memo.new(memo_params)
+    @memo.save!
+    redirect_to memos_path
+  end
+
+  private
+
+  def memo_params
+    params.require(:memo).permit(:title, :content)
+  end
 end
