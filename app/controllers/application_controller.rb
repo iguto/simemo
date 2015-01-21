86918bd9748a1a9ed276f772b48455dee68a4ac1
iguto <clc.iguto@gmail.com>
2015-01-21 15:28:47 +0900

    アプリケーションにBasic認証を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :basic_authenticate
+
+  private
+
+  def basic_authenticate
+    authenticate_or_request_with_http_basic do |user, pass|
+      user == ENV['BASIC_AUTH_USER'] and ENV['BASIC_AUTH_PASSWORD']
+    end
+  end
 end
